3923a1bd8bf0fc5012d932c085a7a7c02881d0eb
Daniel <daniel@kylix.online>
2022-08-04 10:01:19 +0300

    new changes

---
+++ index.js
@@ -0,0 +1,28 @@
+let a = 10;
+let b = 12;
+
+
+
+const add = (a,b) => {
+return a+b;
+}
+const multiply = (a,b) => {
+return a*b;
+}
+const subtract = (a,b) => {
+return a-b;
+}
+const divide = (a,b) => {
+return a/b;
+}
+
+const increment = (a) => {
+   return a++;
+}
+
+const decrement = (a) => {
+  return  a--;
+}
+const makeInt = (n) => {
+    return n
+}
